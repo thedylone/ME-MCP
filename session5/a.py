b02fcbab898fea465b9582dece3178da2d373103
dylan <thedylone@gmail.com>
2022-12-16 16:06:47 +0000

    session 5 task a

---
+++ session5/a.py
@@ -0,0 +1,32 @@
+"""Factorial, again"""
+
+from helpers.task import TaskBase, RangeValidator, get_input, task_to_list
+
+
+class Task(TaskBase):
+    """Factorial, again"""
+
+    tasklist = []
+
+    def factorial(self, n):
+        """Write a function, Factorial, to compute the factorial
+        of an integer number. The function receives an integer number n
+        and returns the value of its factorial."""
+        if type(n) != int or n < 0:
+            raise TypeError("n must be an integer")
+        if n < 2:
+            return 1
+        return n * self.factorial(n - 1)
+
+    @task_to_list(tasklist)
+    def task1(self):
+        """Write a function, Factorial, to compute the factorial
+        of an integer number. The function receives an integer number n
+        and returns the value of its factorial."""
+        num = get_input(int, "Enter an integer", RangeValidator(minval=0))
+        return {"factorial": self.factorial(num)}
+
+
+if __name__ == "__main__":
+    task = Task("A")
+    task.run_tasks()
