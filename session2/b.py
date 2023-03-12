1f08af2e936a4265b00060387caafbe143ddfd13
dylan <85779798+thedylone@users.noreply.github.com>
2023-03-12 08:53:23 +0000

    session 2

--- session2/b.py
+++ session2/b.py
@@ -1,34 +1,39 @@
 """Managing lists"""
 
-import session2.a
 from helpers.task import TaskBase, task_to_list
+from session2.a import Task as TaskA
 
 
 class Task(TaskBase):
     """Managing lists"""
 
-    tasklist = []
-    TaskA = session2.a.Task(output=False)
-    TaskA.run_tasks()
-    A = TaskA.A
-    D = []
+    tasklist: list = []
+
+    def __init__(self, name: str = "", output: bool = True) -> None:
+        super().__init__(name, output)
+        task_a: TaskA = TaskA("A", False)
+        task_a.run_tasks()
+        self.list_a: list[int] = task_a.list_a
+        self.list_d: list[int] = []
 
     @task_to_list(tasklist)
-    def task1(self):
+    def task1(self) -> dict[str, list[int]]:
         """Starting from list A generated in Task A,
         set to zero the values in every third element."""
-        self.A = [0 if i % 3 == 0 else v for i, v in enumerate(self.A)]
-        return {"A": self.A}
+        self.list_a = [
+            0 if i % 3 == 0 else v for i, v in enumerate(self.list_a)
+        ]
+        return {"A": self.list_a}
 
     @task_to_list(tasklist)
-    def task2(self):
+    def task2(self) -> dict[str, list[int]]:
         """Then create a new list D that is
         the reverse (flipped position) of list A."""
-        self.D = self.A[::-1]
-        return {"D": self.D}
+        self.list_d = self.list_a[::-1]
+        return {"D": self.list_d}
 
 
 if __name__ == "__main__":
-    task = Task("B")
+    task: Task = Task("B")
     task.run_tasks()
-    print(task.D[69])
+    print(task.list_d[69])
--- session2/d.py
+++ session2/d.py
@@ -1,7 +1,6 @@
 """Conditional flow"""
 
 import random
-
 from helpers.task import TaskBase, get_input, task_to_list
 
 
@@ -10,7 +9,7 @@ class Player:
 
     def __init__(self, name, score=0) -> None:
         self.name = name
-        self.score = score
+        self.score: int = score
 
     def __str__(self) -> str:
         return self.name
@@ -25,27 +24,27 @@ class Player:
 class Task(TaskBase):
     """Conditional flow"""
 
-    tasklist = []
+    tasklist: list = []
 
     def __init__(self, name="", output=True) -> None:
         super().__init__(name, output)
-        self.X = Player(input("Enter Player X's name: "))
-        self.Y = Player(input("Enter Player Y's name: "))
+        self.player_x: Player = Player(input("Enter Player X's name: "))
+        self.player_y: Player = Player(input("Enter Player Y's name: "))
 
     @task_to_list(tasklist)
-    def task1(self):
+    def task1(self) -> dict[str, str | int]:
         """Two people intend to play dice.
         Input from the keyboard the names of these two people.
         Throw the dice once for each person. Establish who is the winner.
         Display the winner in the format 'Player X wins player Y'."""
-        dice_x = random.randint(1, 6)
-        dice_y = random.randint(1, 6)
+        dice_x: int = random.randint(1, 6)
+        dice_y: int = random.randint(1, 6)
         if dice_x > dice_y:
-            out = 1
-            res = f"{self.X} wins {self.Y}"
+            out: int = 1
+            res: str = f"{self.player_x} wins {self.player_y}"
         elif dice_x < dice_y:
             out = -1
-            res = f"{self.Y} wins {self.X}"
+            res = f"{self.player_y} wins {self.player_x}"
         else:
             out = 0
             res = "Draw"
@@ -53,33 +52,37 @@ class Task(TaskBase):
         return {"out": out, "res": res}
 
     @task_to_list(tasklist)
-    def task2(self):
+    def task2(self) -> dict[str, str]:
         """The same two people throw the dice N times.
         Amend the previous script to allow so
         and count the winning games (not the scores) for each person.
         At the end display the winner in the format
         'Player X won x games, player Y won only y games'."""
-        times = get_input(int, "N")
+        times: int = int(get_input(int, "N"))
         for _ in range(int(times)):
-            roll = self.task1().get("out", 0)
+            roll: str | int = self.task1().get("out", 0)
             if roll == 1:
-                self.X.score += 1
+                self.player_x.score += 1
             elif roll == -1:
-                self.Y.score += 1
+                self.player_y.score += 1
 
-        if int(self.X) >= int(self.Y):
-            win, lose = self.X, self.Y
+        win: Player
+        lose: Player
+        if int(self.player_x) >= int(self.player_y):
+            win, lose = self.player_x, self.player_y
         else:
-            win, lose = self.Y, self.X
-        out = f"{win} won {int(win)} games, {lose} won only {int(lose)} games"
+            win, lose = self.player_y, self.player_x
+        out: str = (
+            f"{win} won {int(win)} games, {lose} won only {int(lose)} games"
+        )
         return {"winner": out}
 
 
 if __name__ == "__main__":
-    task = Task("D")
+    task: Task = Task("D")
     task.run_tasks()
 
-    a = 0
-    c = 1
-    d = 2
-    print((a == c or a > c) or (d < c))
+    A: int = 0
+    C: int = 1
+    D: int = 2
+    print((A == C or A > C) or (D < C))
