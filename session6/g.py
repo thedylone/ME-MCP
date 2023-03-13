63f00b22432f5cf7ebee796ec202456a69e24ba7
dylan <85779798+thedylone@users.noreply.github.com>
2023-03-13 11:12:00 +0000

    session 6

--- session6/g.py
+++ session6/g.py
@@ -6,21 +6,21 @@ from helpers.task import TaskBase, task_to_list, get_input
 class Task(TaskBase):
     """Merge sort (Divide et Conquer)"""
 
-    tasklist = []
+    tasklist: list = []
 
     @staticmethod
-    def mergesort(values):
+    def mergesort(values) -> list[int]:
         """Merge sort a list of values"""
-        length = len(values)
+        length: int = len(values)
         if length == 0:
             return []
         if length == 1:
             return values
-        i = 0
-        j = 0
-        list_a = Task.mergesort(values[: length // 2])
-        list_b = Task.mergesort(values[length // 2 :])
-        output = []
+        i: int = 0
+        j: int = 0
+        list_a: list[int] = Task.mergesort(values[: length // 2])
+        list_b: list[int] = Task.mergesort(values[length // 2 :])
+        output: list[int] = []
         while i < len(list_a) or j < len(list_b):
             if i >= len(list_a):
                 output += list_b[j:]
@@ -42,15 +42,15 @@ class Task(TaskBase):
         return output
 
     @task_to_list(tasklist)
-    def task1(self):
+    def task1(self) -> dict[str, list[int]]:
         """Write a recursive function, MergeSort(List), that receives
         a list of values and returns the same list sorted in ascending values,
         by using the Merge Sort method algorithm."""
-        values = get_input(str, "Enter values separated by commas").split(",")
-        values = [int(val) for val in values]
+        values_str: str = get_input(str, "Enter values separated by commas")
+        values: list[int] = [int(val) for val in values_str.split(",")]
         return {"sorted": Task.mergesort(values)}
 
 
 if __name__ == "__main__":
-    task = Task("G")
+    task: Task = Task("G")
     task.run_tasks()
