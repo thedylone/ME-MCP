772f9c585bfbfb4dd7955d79bad25959d0379d53
dylan <85779798+thedylone@users.noreply.github.com>
2023-03-14 15:01:59 +0000

    session 8

--- session8/d.py
+++ session8/d.py
@@ -1,38 +1,42 @@
 """Using methods"""
 
 from helpers.task import TaskBase, task_to_list
+from session8.a import Cohort, Registry
 from session8.c import Task as TaskC
 
 
 class Task(TaskBase):
     """Using methods"""
 
-    tasklist = []
-    taskC = TaskC(output=False)
-    taskC.run_tasks()
-    list_me = taskC.list_me
-    registry = taskC.registry
+    tasklist: list = []
+
+    def __init__(self, name: str = "", output: bool = True) -> None:
+        super().__init__(name, output)
+        task_c: TaskC = TaskC(output=False)
+        task_c.run_tasks()
+        self.list_me: list[Cohort] = task_c.list_me
+        self.registry: Registry = task_c.registry
 
     @task_to_list(tasklist)
-    def task1(self):
+    def task1(self) -> dict[str, str]:
         """Determine and print out the name of
         the top student for each cohort."""
         return {str(c.year): c.best_student() for c in self.list_me}
 
     @task_to_list(tasklist)
-    def task2(self):
+    def task2(self) -> dict[str, list[str]]:
         """Determine and print the list of names of students
         entering the Dean's List."""
         return {str(c.year): [s.name for s in c.deans()] for c in self.list_me}
 
     @task_to_list(tasklist)
-    def task3(self):
+    def task3(self) -> dict[str, str]:
         """For every student in Mech. Eng., print out the name and the average
         mark classification."""
         return {s.name: s.get_classify() for s in self.registry.enrolments}
 
     @task_to_list(tasklist)
-    def task4(self):
+    def task4(self) -> dict[str, int]:
         """Determine how many students overall in Mech. Eng. have achieved a
         First, a 2.1 and a 2.2, based on their marks average."""
         return {
@@ -43,5 +47,5 @@ class Task(TaskBase):
 
 
 if __name__ == "__main__":
-    task = Task("D")
+    task: Task = Task("D")
     task.run_tasks()
